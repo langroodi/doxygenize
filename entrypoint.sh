c386cdd0a20c847599081952a0a612e469747255
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2021-09-18 23:28:19 +0200

    Make the action zero argument.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,33 +1,28 @@
 #!/bin/sh
 
-# Fetch the first argument (GitHub Actor)
-USERNAME="action"
-echo "Global GIT user-name: $USERNAME"
-USEREMAIL="action@users.noreply.github.com"
-echo "Global GIT user-email: $USEREMAIL"
+# Fetch the first argument (Doxygen configuratuin file path)
+DOXYGENCONF="./doc/doxygen.conf"
+echo "Doxygen confiugration file path: $DOXYGENCONF"
 
-# Fetch the second argument (GitHub Token) 
-TOKEN=$2
-
-# Fetch the third agument (GitHub Repository)
-REPOSITORY=$3
-echo "Target repository $REPOSITORY"
+# Fetch the second agument (Generated HTML documents output folder)
+HTMLOUTPUT=$2
+echo "Generated HTML documents output folder $HTMLOUTPUT"
 
 # Install Doxygen, GIT, and OpenSSH packages
 apk add doxygen git openssh
 
 # Generate code documentation
-doxygen ./doc/doxygen.conf
+doxygen $DOXYGENCONF
 
 # Set GIT global user configuration
-git config --global user.email "$USEREMAIL"
-git config --global user.name "$USERNAME"
+git config user.name github-actions
+git config user.email github-actions@github.com
 
 # Add the generated code documentation to the GIT even they are ignored
-git add --force ./doc/html
+git add --force $HTMLOUTPUT
 
 # Stash the generated code documentation
-git stash save ./doc/html
+git stash save $HTMLOUTPUT
 
 # Synchronize with the remote repository
 git remote update
@@ -42,17 +37,13 @@ git rm -rf .
 git stash pop
 
 # Move the the generated code documentation to the branch root
-mv ./doc/html/* .
+mv $HTMLOUTPUT/* .
 
 # Add all the changes to the GIT
 git add --all
 
 # Commit all the changed to the the GitHub Pages branch
 git commit -m "Auto commit."
-
-# Set repository remote URL
-REMOTEURL=https://${USERNAME}:${TOKEN}@github.com/${REPOSITORY}.git
-#git remote set-url $REMOTEURL
 
 # Push the changes to the remote GitHub Pages branch
 git push

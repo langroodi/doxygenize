3561c1a509bc53ac3bd032430b89d610dd44f02f
Armin Kassemi Langroodi <mohandes.zalatan@gmail.com>
2022-05-01 15:02:57 +0200

    Update entrypoint.sh
    Stop the execution flow if GitHub Pages commit failed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -95,7 +95,7 @@ CommitChanges () {
     git add --force "$DESTINATIONDIR"
     
     # Commit all the changed to the the GitHub Pages branch
-    git commit -m "Auto commit"
+    git commit -m "Auto commit" || exit 1
     
     # Push the changes to the remote GitHub Pages branch
     git push
